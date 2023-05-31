5c1beabc772554bc2f7cc97f7abb8cdf694cded4
davit <davit.natenadze@gmail.com>
2023-05-31 12:05:03 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Davit Natenadze on 31.05.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
